fad3928e4be17b8bd631a061d12605e455ea340f
iheliang <heliang399@gmail.com>
2024-01-17 21:37:26 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <Search />
+    <Lists />
+  </div>
+</template>
+
+<script>
+import Search from "./components/Search.vue";
+import Lists from "./components/Lists.vue";
+export default {
+  name: "App",
+  components: {
+    Search,
+    Lists,
+  },
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+    }
+  },
+  lintOnSave: false,//关闭语法检查
+  // 开启代理服务器（方式一）
+  // devServer: {
+  //   proxy: 'http://localhost:5000'
+  // },
+  // 开启代理服务器（方式二）
+  devServer: {
+    proxy: {
+      '/atguigu': {
+        target: 'http://localhost:5000',
+        pathRewrite: { "^/atguigu": "" },
+        ws: true, //用于支持websocket
+        changeOrigin: true //用于控制请求头中的host值
+        // },
+        // '^/foo': {
+        //   target: '<other_url>'
+      },
+      '/demo': {
+        target: 'http://localhost:5001',
+        pathRewrite: { "^/demo": "" },
+        ws: true, //用于支持websocket
+        changeOrigin: true //用于控制请求头中的host值
+        // },
+        // '^/foo': {
+        //   target: '<other_url>'
+      }
+    }
+  }
+}
